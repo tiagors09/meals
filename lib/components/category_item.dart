7403357050db43bb25821eb4983dff9ce4c5c11b
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-21 20:48:54 -0300

    category_item.dart: added style to category item

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -12,6 +12,18 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(25),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
       child: Text(category.title),
     );
   }
