a975c60df2ab550d3ad871220f8292a9ee77394b
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-22 10:19:00 -0300

    category_item.dart: added ink well

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -11,22 +11,27 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(25),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).colorScheme.primary,
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
       ),
     );
   }
