b748eb34b4adf0a71cd45409f9f2533e1c31f022
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-22 10:18:30 -0300

    categories_screen.dart: changed aspect ration and added a padding

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -12,9 +12,10 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Vamos Cozinhar?'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
-          childAspectRatio: 1 / 2,
+          childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
