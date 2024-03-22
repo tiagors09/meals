eae5777438ced529a02257906b03a61fab6b6c44
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-22 10:18:42 -0300

    added cateogries meals screen

---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  const CategoriesMealsScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Receitas'),
+      ),
+      body: const Center(
+        child: Text('Receitas por categoria'),
+      ),
+    );
+  }
+}
