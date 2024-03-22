dfc67a556dcd424109b8fc15158f72fb8947a0dd
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-22 14:26:02 -0300

    category_item.dart: refactored route using named route and enum

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
-import 'package:meals/screens/categories_meals_screen.dart';
+import 'package:meals/utils/app_routes.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
@@ -11,10 +11,16 @@ class CategoryItem extends StatelessWidget {
   });
 
   void _selectCategory(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (_) {
-        return const CategoriesMealsScreen();
-      }),
+    // Navigator.of(context).push(
+    //   MaterialPageRoute(builder: (_) {
+    //     return CategoriesMealsScreen(
+    //       category: category,
+    //     );
+    //   }),
+    // );
+    Navigator.of(context).pushNamed(
+      AppRoutes.categoriesMeals.name,
+      arguments: category,
     );
   }
 
