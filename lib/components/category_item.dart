124e31150c5c0a9067b8ff1ef656c218af911348
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-21 22:29:20 -0300

    category_item.dart: added title text style

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -24,7 +24,10 @@ class CategoryItem extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Text(category.title),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
     );
   }
 }
