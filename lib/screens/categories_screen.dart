d4d0f62c8c6443499899f8522f7558b1c5052207
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-26 19:40:14 -0300

    categories_screen.dart: removed scaffold

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,24 +7,19 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Vamos Cozinhar?'),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: dummyCategories
-            .map(
-              (c) => CategoryItem(c),
-            )
-            .toList(),
-      ),
+      children: dummyCategories
+          .map(
+            (c) => CategoryItem(c),
+          )
+          .toList(),
     );
   }
 }
