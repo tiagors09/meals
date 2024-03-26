55cbb872fa2c19bc84476e7e410caf7a8e2bd34e
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-26 20:15:04 -0300

    tabs_screen.dart: changed to bottom nav bar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,35 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
 
   @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Map<String, Object>> _screens = [
+    {
+      'title': 'Lista de Categorias',
+      'screen': const CategoriesScreen(),
+    },
+    {
+      'title': 'Meus Favoritos',
+      'screen': const FavoriteScreen(),
+    },
+  ];
+
+  final List<BottomNavigationBarItem> _items = [
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.category),
+      label: 'Categorias',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.star),
+      label: 'Favoritos',
+    ),
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    const tabs = [
-      Tab(
-        icon: Icon(Icons.category),
-        text: 'Categorias',
-      ),
-      Tab(
-        icon: Icon(Icons.star),
-        text: 'Favoritos',
-      ),
-    ];
-
-    const screens = [
-      CategoriesScreen(),
-      FavoriteScreen(),
-    ];
-
-    return DefaultTabController(
-      length: tabs.length,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Vamos Cozinhar?'),
-          bottom: const TabBar(tabs: tabs),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _screens[_selectedScreenIndex]['title'] as String,
         ),
-        body: const TabBarView(children: screens),
+      ),
+      body: _screens[_selectedScreenIndex]['screen'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedScreenIndex,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        items: _items,
       ),
     );
   }
