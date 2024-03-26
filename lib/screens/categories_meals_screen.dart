34a31af4882e91bc64efe0c15aaa6cbdf7ef68f3
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-26 11:36:25 -0300

    categories_meals_screen.dart: added a list view to show meals

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,7 +1,10 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:meals/components/meal_item.dart';
 import 'package:meals/models/category.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   const CategoriesMealsScreen({
@@ -12,12 +15,25 @@ class CategoriesMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
 
+    final categoryMeals = dummyMeals
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: const Center(
-        child: Text('Receitas por categoria'),
+      body: Center(
+        child: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return MealItem(
+              meal: categoryMeals[index],
+            );
+          },
+        ),
       ),
     );
   }
