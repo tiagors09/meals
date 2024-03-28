ee747ff11e45291b3a7c443164a5ecb7cad21a0a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-28 11:24:24 -0300

    tabs_screen.dart: added main drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/main_drawer.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
@@ -48,6 +49,7 @@ class _TabsScreenState extends State<TabsScreen> {
           _screens[_selectedScreenIndex]['title'] as String,
         ),
       ),
+      drawer: const MainDrawer(),
       body: _screens[_selectedScreenIndex]['screen'] as Widget,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedScreenIndex,
