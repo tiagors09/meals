2eda051501566ac1d59d39d10b999c7a6edd13e2
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-30 12:25:38 -0300

    favorite_screen.dart: showing fav meals

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+import '../models/meal.dart';
 
 class FavoriteScreen extends StatelessWidget {
-  const FavoriteScreen({super.key});
+  final List<Meal> favoriteMeals;
+
+  const FavoriteScreen({
+    super.key,
+    required this.favoriteMeals,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Minhas refeições favoritas'),
-    );
+    if (favoriteMeals.isEmpty) {
+      return const Center(
+        child: Text('Minhas refeições favoritas'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) => MealItem(
+          meal: favoriteMeals[index],
+        ),
+        itemCount: favoriteMeals.length,
+      );
+    }
   }
 }
