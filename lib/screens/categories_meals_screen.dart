f3cb00f24e9e7b3ed4052fba77236f873eeee19f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-03-30 11:25:44 -0300

    categories_meals_screen.dart: refatored meal name class

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:meals/components/meal_item.dart';
-import 'package:meals/models/category.dart';
-import '../data/dummy_data.dart';
+import '../components/meal_item.dart';
+import '../models/category.dart';
+import '../models/meal.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final List<Meal> meals;
+
   const CategoriesMealsScreen({
     super.key,
+    required this.meals,
   });
 
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
 
-    final categoryMeals = dummyMeals
+    final categoryMeals = meals
         .where(
           (meal) => meal.categories.contains(category.id),
         )
